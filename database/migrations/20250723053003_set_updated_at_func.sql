82e7666e530ea7910b2cb148ef9bb7bcfeb8c043
rohan <rohanverma031@gmail.com>
2025-07-23 11:06:13 +0530

    update: changed now() to clock_timestamp() for updated_at field and removed supabase auth.updated_by field

--- database/migrations/20250723053003_set_updated_at_func.sql
+++ database/migrations/20250723053003_set_updated_at_func.sql
@@ -4,8 +4,7 @@
 create or replace function set_updated_columns()
 returns trigger as $$
 begin
-  new.updated_at = now();
-  new.updated_by = auth.uid();
+  new.updated_at = clock_timestamp();
   return new;
 end;
 $$ language plpgsql;
