adf8a56d4c1f7c5f275086144465286e28fabbbd
rohan <rohanverma031@gmail.com>
2025-07-24 19:26:29 +0530

    feat: added dotEnv for local .env files

--- main.go
+++ main.go
@@ -4,19 +4,28 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/Adgytec/adgytec-flow/config/server"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("error loading .env file")
+	}
+
+	port := os.Getenv("port")
+
 	rootCtx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
-	httpServer := server.CreateHttpServer("8080")
+	httpServer := server.CreateHttpServer(port)
 	go func() {
-		log.Println("Server starting on :8080.")
+		log.Printf("Server starting on port %s.", port)
 		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			panic(err)
 		}
