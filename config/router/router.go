99d13306e8942cc2d6ad7dc65bb6832a9760b1d5
rohan <rohanverma031@gmail.com>
2025-07-31 20:22:14 +0530

    feat: added router middlewares

--- config/router/router.go
+++ config/router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/Adgytec/adgytec-flow/config/app"
 	"github.com/Adgytec/adgytec-flow/services/access_management"
@@ -9,6 +10,8 @@ import (
 	"github.com/Adgytec/adgytec-flow/utils/helpers"
 	"github.com/Adgytec/adgytec-flow/utils/payload"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 type serviceFactory func(params app.IApp) core.IServiceMux
@@ -39,6 +42,30 @@ func handle400(mux *chi.Mux) {
 
 func CreateApplicationRouter(appConfig app.IApp) *chi.Mux {
 	mux := chi.NewMux()
+
+	mux.Use(middleware.Heartbeat("/health"))
+	mux.Use(middleware.Logger)
+	mux.Use(middleware.Recoverer)
+	mux.Use(middleware.StripSlashes)
+	mux.Use(middleware.AllowContentType("application/json"))
+
+	allowedOrigins := []string{
+		"https://*",
+	}
+
+	if os.Getenv("ENV") == "development" {
+		allowedOrigins = append(allowedOrigins, "http://*")
+	}
+
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   allowedOrigins,
+		AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
 	handle400(mux)
 
 	for _, factory := range services {
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/config v1.29.18
 	github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider v1.53.5
 	github.com/go-chi/chi/v5 v5.2.2
+	github.com/go-chi/cors v1.2.2
 	github.com/jackc/pgx/v5 v5.7.5
 	github.com/joho/godotenv v1.5.1
 )
@@ -23,7 +24,6 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.30.4 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.34.1 // indirect
 	github.com/aws/smithy-go v1.22.4 // indirect
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
