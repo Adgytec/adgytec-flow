c21a69591992fb23179819369e2e9e2544eb397e
rohan <rohanverma031@gmail.com>
2025-07-31 20:12:39 +0530

    feat: added custom responses for not found and method not allowed

--- config/router/router.go
+++ config/router/router.go
@@ -1,9 +1,41 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/Adgytec/adgytec-flow/config/app"
+	"github.com/Adgytec/adgytec-flow/services/access_management"
+	"github.com/Adgytec/adgytec-flow/utils/core"
+	"github.com/Adgytec/adgytec-flow/utils/helpers"
+	"github.com/Adgytec/adgytec-flow/utils/payload"
 	"github.com/go-chi/chi/v5"
 )
+
+type serviceFactory func(params app.IApp) core.IServiceMux
+
+var services = []serviceFactory{
+	func(appConfig app.IApp) core.IServiceMux {
+		return access_management.CreateAccessManagementMux(appConfig)
+	},
+}
+
+func handle400(mux *chi.Mux) {
+	mux.NotFound(func(w http.ResponseWriter, _ *http.Request) {
+		payload.EncodeJSON(w, http.StatusNotFound, core.ResponseHTTPError{
+			Message: helpers.StringPtr(
+				http.StatusText(http.StatusNotFound),
+			),
+		})
+	})
+
+	mux.MethodNotAllowed(func(w http.ResponseWriter, _ *http.Request) {
+		payload.EncodeJSON(w, http.StatusMethodNotAllowed, core.ResponseHTTPError{
+			Message: helpers.StringPtr(
+				http.StatusText(http.StatusMethodNotAllowed),
+			),
+		})
+	})
+}
 
 func CreateApplicationRouter(appConfig app.IApp) *chi.Mux {
 	mux := chi.NewMux()
