48f30efeee6a9a22d802e1469789e354b9afc7a1
rohan <rohanverma031@gmail.com>
2025-08-13 15:37:34 +0530

    feat: added pagination response type

--- utils/core/type_http_resopnses.go
+++ utils/core/type_http_resopnses.go
@@ -5,3 +5,15 @@ type ResponseHTTPError struct {
 	Message        *string            `json:"message,omitempty"`
 	FieldErrors    *map[string]string `json:"fieldErrors,omitempty"`
 }
+
+type PageInfo struct {
+	HasNextPage bool   `json:"hasNextPage"`
+	NextCursor  string `json:"nextCursor"`
+	HasPrevPage bool   `json:"hasPrevPage"`
+	PrevCursor  string `json:"prevCursor"`
+}
+
+type ResponsePagination[T any] struct {
+	PageInfo  PageInfo `json:"pageInfo"`
+	PageItems []T      `json:"pageItems"`
+}
