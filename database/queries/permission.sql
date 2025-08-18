c51015c50542840533ca84e7ceb4911b39dfaa95
rohan <rohanverma031@gmail.com>
2025-08-18 19:41:47 +0530

    feat: added column to identify if the permission can be assigned to api key

--- database/queries/permission.sql
+++ database/queries/permission.sql
@@ -21,11 +21,20 @@ INSERT INTO
 		service_id,
 		name,
 		description,
-		required_resources
+		required_resources,
+		api_key_assignable
 	)
 VALUES
-	($1, $2, $3, $4, $5)
+	(
+		$1,
+		$2,
+		$3,
+		$4,
+		$5,
+		$6
+	)
 ON CONFLICT (key) DO UPDATE
 SET
 	name = excluded.name,
-	description = excluded.description;
+	description = excluded.description,
+	api_key_assignable = excluded.api_key_assignable;
