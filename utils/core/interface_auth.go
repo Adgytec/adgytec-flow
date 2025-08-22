50d4319f44f43c1cce4d789668f19b8a388944e9
rohan <rohanverma031@gmail.com>
2025-08-22 20:00:32 +0530

    update: added description for validate api key method

--- utils/core/interface_auth.go
+++ utils/core/interface_auth.go
@@ -7,5 +7,8 @@ type IAuth interface {
 	DisableUser(string) error
 	EnableUser(string) error
 	ValidateUserAccessToken(string) (uuid.UUID, error)
+
+	// this only checks if the API key is in required format as described in the application doc
+	// further validation like if this api key actually exists is done later on
 	ValidateAPIKey(string) (uuid.UUID, error)
 }
