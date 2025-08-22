b0175f0f5b5f4bf8c8da32c636b340376fcaba74
rohan <rohanverma031@gmail.com>
2025-08-22 19:55:53 +0530

    feat: added method to user service pc to get user current status

--- services/user/service_pc.go
+++ services/user/service_pc.go
@@ -1,13 +1,21 @@
 package user
 
 import (
+	"context"
 	"log"
 
+	db_actions "github.com/Adgytec/adgytec-flow/database/actions"
 	"github.com/Adgytec/adgytec-flow/utils/core"
+	"github.com/google/uuid"
 )
 
 type userServicePC struct {
 	service *userService
+}
+
+func (pc *userServicePC) GetUserStatus(ctx context.Context, userID uuid.UUID) db_actions.GlobalUserStatus {
+	// TODO: will implement this later
+	return db_actions.GlobalUserStatusDisabled
 }
 
 func CreateUserServicePC(params iUserServiceParams) core.IUserServicePC {
--- utils/core/interface_user_service.go
+++ utils/core/interface_user_service.go
@@ -3,9 +3,11 @@ package core
 import (
 	"context"
 
+	db_actions "github.com/Adgytec/adgytec-flow/database/actions"
 	"github.com/google/uuid"
 )
 
 type IUserServicePC interface {
 	CreateUser(context.Context, string) (uuid.UUID, error)
+	GetUserStatus(context.Context, uuid.UUID) db_actions.GlobalUserStatus
 }
