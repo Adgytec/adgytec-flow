abae8a29dde4e191acd6ac50b96bba7a8d6cc510
rohan <rohanverma031@gmail.com>
2025-08-23 15:49:48 +0530

    feat: added new middleware func to check actor Management access check

--- utils/core/interface_middleware.go
+++ utils/core/interface_middleware.go
@@ -17,5 +17,7 @@ type IMiddlewarePC interface {
 	// this checks whether actor is part of the organization whose resources its working on
 	// these methods also checks whether the actor status for that organization, but its only used for individual organiztion related methods
 	ActorOrganizationAccessCheck(http.Handler) http.Handler
-	ActorOrganizationManagementCheck(http.Handler) http.Handler
+	ActorOrganizationManagementAccessCheck(http.Handler) http.Handler
+
+	ActorManagementAccessCheck(http.Handler) http.Handler
 }
