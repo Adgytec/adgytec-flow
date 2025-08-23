846fa0b1b80424849e6a1aeea38741c74cb4c1c6
rohan <rohanverma031@gmail.com>
2025-08-23 22:18:31 +0530

    feat: added helpers to get indvidual actor id and actor type from context

--- utils/helpers/actors.go
+++ utils/helpers/actors.go
@@ -38,3 +38,15 @@ func GetActorDetailsFromContext(ctx context.Context) (core.ActorDetails, error) 
 		Type: actorTypeValue,
 	}, nil
 }
+
+// as both the actor id and actor type are closely releated
+// if any one of them causes any error than both the values are considered invalid
+func GetActorIdFromContext(ctx context.Context) (uuid.UUID, error) {
+	actorDetails, actorDetailsErr := GetActorDetailsFromContext(ctx)
+	return actorDetails.ID, actorDetailsErr
+}
+
+func GetActorTypeFromContext(ctx context.Context) (core.ActorType, error) {
+	actorDetails, actorDetailsErr := GetActorDetailsFromContext(ctx)
+	return actorDetails.Type, actorDetailsErr
+}
