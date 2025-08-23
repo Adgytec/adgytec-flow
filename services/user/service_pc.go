d224e1a7d295df48a40a2a45f912ce18c242d51c
rohan <rohanverma031@gmail.com>
2025-08-23 15:55:53 +0530

    refactor: refactored user service

--- services/user/service_pc.go
+++ services/user/service_pc.go
@@ -1,21 +1,13 @@
 package user
 
 import (
-	"context"
 	"log"
 
-	db_actions "github.com/Adgytec/adgytec-flow/database/actions"
 	"github.com/Adgytec/adgytec-flow/utils/core"
-	"github.com/google/uuid"
 )
 
 type userServicePC struct {
 	service *userService
-}
-
-func (pc *userServicePC) GetUserStatus(ctx context.Context, userID uuid.UUID) (db_actions.GlobalUserStatus, error) {
-	// TODO: will implement this later
-	return db_actions.GlobalUserStatusDisabled, nil
 }
 
 func CreateUserServicePC(params iUserServiceParams) core.IUserServicePC {
