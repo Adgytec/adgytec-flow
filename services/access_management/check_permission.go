c5d8c99f2c2332822863b258f46f97db0238b2bc
rohan <rohanverma031@gmail.com>
2025-08-30 11:05:30 +0530

    update: updated access management check permission method

--- services/access_management/check_permission.go
+++ services/access_management/check_permission.go
@@ -8,7 +8,7 @@ import (
 	"github.com/Adgytec/adgytec-flow/utils/helpers"
 )
 
-func (pc *accessManagementPC) CheckPermission(ctx context.Context, permissionRequired core.PermissionRequired) error {
+func (pc *accessManagementPC) CheckPermission(ctx context.Context, permissionRequired []core.IPermissionRequired) error {
 	actorDetails, actorDetailsErr := helpers.GetActorDetailsFromContext(ctx)
 	if actorDetailsErr != nil {
 		return actorDetailsErr
@@ -18,10 +18,25 @@ func (pc *accessManagementPC) CheckPermission(ctx context.Context, permissionReq
 		ID:         actorDetails.ID,
 		EntityType: actorDetails.Type,
 	}
-	return pc.service.checkPermission(ctx, permissionEntity, permissionRequired)
+
+	// if permissionRequired is empty slice than action is unknown
+	var err error = &app_errors.PermissionDeniedError{
+		Action: "unknown",
+	}
+
+	for _, perm := range permissionRequired {
+		err = pc.service.checkPermission(ctx, permissionEntity, perm)
+
+		// permission successfully resolved when any of the permission is resolved successfully
+		if err == nil {
+			return nil
+		}
+	}
+
+	return err
 }
 
-func (s *accessManagement) checkPermission(ctx context.Context, entity core.PermissionEntity, requiredPermission core.PermissionRequired) error {
+func (s *accessManagement) checkPermission(ctx context.Context, entity core.PermissionEntity, requiredPermission core.IPermissionRequired) error {
 	// TODO: Implement actual permission checking logic here.
 	// For now, returning an error to prevent accidental use in production.
 	return app_errors.ErrNotImplemented
