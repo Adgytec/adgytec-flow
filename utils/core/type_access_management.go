004f095dfeabc638a394d08dd84668eb44f56e15
rohan <rohanverma031@gmail.com>
2025-08-30 10:10:39 +0530

    feat: added self permission struct for defining self permissions

--- utils/core/type_access_management.go
+++ utils/core/type_access_management.go
@@ -5,6 +5,15 @@ import (
 	"github.com/google/uuid"
 )
 
+// SelfPermissions defines the permission type used to define self actions
+// this is not stored in db
+// SelfPermissions are not assignable to any user and are implictly available for all the users for their account actions
+type SelfPermissions struct {
+	Key         string
+	Name        string
+	Description string
+}
+
 type PermissionEntity struct {
 	ID         uuid.UUID
 	EntityType db_actions.GlobalActorType
