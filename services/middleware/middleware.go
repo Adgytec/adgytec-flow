c934725e2d449563181e5af95ddf3666310b2160
rohan <rohanverma031@gmail.com>
2025-09-03 15:34:32 +0530

    refactor: refactored middleware to remove prefix i from interface names

--- services/middleware/middleware.go
+++ services/middleware/middleware.go
@@ -2,7 +2,7 @@ package app_middleware
 
 import "github.com/Adgytec/adgytec-flow/utils/core"
 
-type iAppMiddlewareParams interface {
+type appMiddlewareParams interface {
 	Auth() core.Auth
 	UserService() core.UserServicePC
 }
@@ -12,7 +12,7 @@ type appMiddleware struct {
 	userService core.UserServicePC
 }
 
-func newAppMiddleware(params iAppMiddlewareParams) *appMiddleware {
+func newAppMiddleware(params appMiddlewareParams) *appMiddleware {
 	return &appMiddleware{
 		auth:        params.Auth(),
 		userService: params.UserService(),
