1218073ece5b18643bdca9272b42d42a65a887d4
rohan <rohanverma031@gmail.com>
2025-09-03 19:08:12 +0530

    refactor: moved user service interface to user package

--- services/user/service_pc.go
+++ services/user/service_pc.go
@@ -1,16 +1,23 @@
 package user
 
 import (
+	"context"
 	"log"
 
-	"github.com/Adgytec/adgytec-flow/utils/core"
+	db_actions "github.com/Adgytec/adgytec-flow/database/actions"
+	"github.com/google/uuid"
 )
+
+type UserServicePC interface {
+	NewUser(context.Context, string) (uuid.UUID, error)
+	GetUserStatus(context.Context, uuid.UUID) (db_actions.GlobalUserStatus, error)
+}
 
 type userServicePC struct {
 	service *userService
 }
 
-func NewUserServicePC(params userServiceParams) core.UserServicePC {
+func NewUserServicePC(params userServiceParams) UserServicePC {
 	log.Println("creating user-service PC")
 	return &userServicePC{
 		service: newUserService(params),
