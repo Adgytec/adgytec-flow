cbc2e1486ebb1fb29b68ee7be34825a08882e59d
rohan <rohanverma031@gmail.com>
2025-09-06 22:02:25 +0530

    feat: added funcs to get data based on request paramters

--- utils/pagination/fetch.go
+++ utils/pagination/fetch.go
@@ -1,1 +1,53 @@
 package pagination
+
+import (
+	"context"
+)
+
+// GetPaginatedData T defines db response type and M defines model used in application
+// this is the function called by the service which require paginated response
+func GetPaginatedData[T any, M any](
+	ctx context.Context,
+	reqParams PaginationRequestParams,
+	actions PaginationActions[T, M],
+) (*ResponsePagination[M], error) {
+	return nil, nil
+}
+
+func getPageByQuery[T any, M any](
+	ctx context.Context,
+	searchQuery string,
+	actions *PaginationActions[T, M],
+) (ResponsePagination[M], error) {
+	var zero ResponsePagination[M]
+	return zero, nil
+}
+
+func getInitialPage[T any, M any](
+	ctx context.Context,
+	sort PaginationRequestSorting,
+	actions *PaginationActions[T, M],
+) (ResponsePagination[M], error) {
+	var zero ResponsePagination[M]
+	return zero, nil
+}
+
+func getNextPage[T any, M any](
+	ctx context.Context,
+	nextCursor string,
+	sort PaginationRequestSorting,
+	actions *PaginationActions[T, M],
+) (ResponsePagination[M], error) {
+	var zero ResponsePagination[M]
+	return zero, nil
+}
+
+func getPrevPage[T any, M any](
+	ctx context.Context,
+	prevCursor string,
+	sort PaginationRequestSorting,
+	actions *PaginationActions[T, M],
+) (ResponsePagination[M], error) {
+	var zero ResponsePagination[M]
+	return zero, nil
+}
