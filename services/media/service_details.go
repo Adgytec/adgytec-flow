68e5b3467a26b7492eed5bb794a697c568fc923b
rohan <rohanverma031@gmail.com>
2025-09-09 22:27:59 +0530

    update: updated media service init.go

--- services/media/service_details.go
+++ services/media/service_details.go
@@ -1,13 +1,15 @@
 package media
 
-import "github.com/Adgytec/adgytec-flow/database/db"
+import (
+	"github.com/Adgytec/adgytec-flow/database/db"
+	"github.com/Adgytec/adgytec-flow/utils/core"
+)
 
 var serviceName = "media"
 
 var mediaServiceDetails = db.AddServiceParams{
-ID:               core.GetIDFromPayload([]byte(serviceName)),
+	ID:               core.GetIDFromPayload([]byte(serviceName)),
 	Name:             serviceName,
 	Assignable:       false,
 	LogicalPartition: db.GlobalServiceLogicalPartitionTypeNone,
-
 }
