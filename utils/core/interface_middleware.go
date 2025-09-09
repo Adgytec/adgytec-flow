8de256c3966223256f09ae15847b592ae97ad481
rohan <rohanverma031@gmail.com>
2025-09-09 20:14:52 +0530

    update: added descriptive comments in middleware pc

--- utils/core/interface_middleware.go
+++ utils/core/interface_middleware.go
@@ -7,15 +7,19 @@ import "net/http"
 // As API key can be generated for both a single organization and for management purposes
 // need multiple handler for api keys to correctly verify for the required action
 type MiddlewarePC interface {
+	// verify and adds actor details to request context
 	ValidateAndGetActorDetailsFromHttpRequest(http.Handler) http.Handler
 
+	// ensures actor type user can only access the next routes
 	EnsureActorTypeUserOnly(http.Handler) http.Handler
 
 	// check auth status of the actor type user
 	ValidateActorTypeUserGlobalStatus(http.Handler) http.Handler
 
+	// validates organization current status and check if request method is allowed
 	CheckOrganizationStatusAndRequestMethod(http.Handler) http.Handler
 
+	// check if current actor is part of management access
 	ActorManagementAccessCheck(http.Handler) http.Handler
 
 	// this checks whether actor is part of the organization whose resources its working on
