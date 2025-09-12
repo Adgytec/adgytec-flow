93e7b58553523f1540ed59e75937e413ccae7387
rohan <rohanverma031@gmail.com>
2025-09-12 22:11:20 +0530

    feat: added trigger function for adding expires at value

--- database/migrations/20250730041701_set_time_fields_default.sql
+++ database/migrations/20250730041701_set_time_fields_default.sql
@@ -14,9 +14,22 @@ begin
 end;
 $$ language plpgsql;
 
+CREATE OR REPLACE FUNCTION global.set_expires_at () returns trigger AS $$
+begin
+    IF TG_NARGS > 0 THEN
+        new.expires_at = clock_timestamp() + (TG_ARGV[0])::interval;
+    ELSE
+        new.expires_at = clock_timestamp() + interval '24 hours';
+    END IF;
+    return new;
+end;
+$$ language plpgsql;
+
 -- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
+DROP FUNCTION if EXISTS global.set_expires_at;
+
 DROP FUNCTION if EXISTS global.set_created_at;
 
 DROP FUNCTION if EXISTS global.set_updated_at;
