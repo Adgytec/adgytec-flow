779e39f949bb4283c5afd30ddabe5f220fdc14ee
rohan <rohanverma031@gmail.com>
2025-09-14 17:17:31 +0530

    update: removed media service details

--- services/media/service_details.go
+++ services/media/service_details.go
@@ -1,10 +1,3 @@
 package media
 
 var serviceName = "media"
-
-// var mediaServiceDetails = db.AddServiceParams{
-// 	ID:               core.GetIDFromPayload([]byte(serviceName)),
-// 	Name:             serviceName,
-// 	Assignable:       false,
-// 	LogicalPartition: db.GlobalServiceLogicalPartitionTypeNone,
-// }
