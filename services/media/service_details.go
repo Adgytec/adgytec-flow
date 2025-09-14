1a48de379a9d6868b0a99505024425c5a9e745a2
rohan <rohanverma031@gmail.com>
2025-09-14 17:20:47 +0530

    feat: added descriptive comment about media service details

--- services/media/service_details.go
+++ services/media/service_details.go
@@ -1,3 +1,6 @@
 package media
 
+// Media service is secondary service which provides the simple methods to upload media items
+// It doesnt' require any permissions as media uploads are context dependant and will be handled by primary service action
+// It also doesn't require its details in database
 var serviceName = "media"
