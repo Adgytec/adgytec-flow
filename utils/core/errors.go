32198bbcaf58522d57dd73cc4874b13488c46a0d
rohan <rohanverma031@gmail.com>
2025-09-15 16:46:05 +0530

    feat: added FieldValidationError to handle request body validation errors

--- utils/core/errors.go
+++ utils/core/errors.go
@@ -1,7 +1,44 @@
 package core
 
-import "errors"
+import (
+	"errors"
+	"net/http"
+
+	"github.com/Adgytec/adgytec-flow/utils/apires"
+	"github.com/Adgytec/adgytec-flow/utils/pointer"
+)
 
 var (
-	ErrNotImplemented = errors.New("error not implemented")
+	ErrNotImplemented  = errors.New("error not implemented")
+	ErrFieldValidation = errors.New("invalid field values")
 )
+
+type FieldValidationError struct {
+	ValidationErrors error
+}
+
+func (e *FieldValidationError) Error() string {
+	if e.ValidationErrors == nil {
+		return ErrFieldValidation.Error()
+	}
+
+	return e.ValidationErrors.Error()
+}
+
+func (e *FieldValidationError) Is(target error) bool {
+	return target == ErrFieldValidation
+}
+
+func (e *FieldValidationError) HTTPResponse() apires.ErrorDetails {
+	if e.ValidationErrors == nil {
+		return apires.ErrorDetails{
+			HTTPStatusCode: http.StatusBadRequest,
+			Message:        pointer.New(e.Error()),
+		}
+	}
+
+	return apires.ErrorDetails{
+		HTTPStatusCode: http.StatusUnprocessableEntity,
+		FieldErrors:    e.ValidationErrors,
+	}
+}
