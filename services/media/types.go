5536029c70f3c86895862a9e730d8fff36775ab4
rohan <rohanverma031@gmail.com>
2025-09-15 09:24:27 +0530

    update: made new media item input struct method to be value receivers

--- services/media/types.go
+++ services/media/types.go
@@ -6,13 +6,13 @@ type NewMediaItemInput struct{}
 
 // Validate() validates the input values
 // implementation details will be added later
-func (mediaItemInput *NewMediaItemInput) Validate() error {
+func (mediaItemInput NewMediaItemInput) Validate() error {
 	return core.ErrNotImplemented
 }
 
 // EnsureMediaItemIsImage() ensures the item that will be uploaded is image
 // implementation will be added later
-func (mediaItemInput *NewMediaItemInput) EnsureMediaItemIsImage() error {
+func (mediaItemInput NewMediaItemInput) EnsureMediaItemIsImage() error {
 	validationErr := mediaItemInput.Validate()
 	if validationErr != nil {
 		return validationErr
@@ -23,7 +23,7 @@ func (mediaItemInput *NewMediaItemInput) EnsureMediaItemIsImage() error {
 
 // EnsureMediaItemIsVideo() ensures the item that will be uploaded is video
 // implementation will be added later
-func (mediaItemInput *NewMediaItemInput) EnsureMediaItemIsVideo() error {
+func (mediaItemInput NewMediaItemInput) EnsureMediaItemIsVideo() error {
 	validationErr := mediaItemInput.Validate()
 	if validationErr != nil {
 		return validationErr
