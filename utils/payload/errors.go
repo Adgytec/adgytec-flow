c708f09cc239b9eaf4f194be16653430fc3fa93b
rohan <rohanverma031@gmail.com>
2025-09-15 16:53:32 +0530

    update: removed unnecessary request validation error from payload package

--- utils/payload/errors.go
+++ utils/payload/errors.go
@@ -2,7 +2,6 @@ package payload
 
 import (
 	"errors"
-	"net/http"
 
 	"github.com/Adgytec/adgytec-flow/utils/apires"
 	"github.com/Adgytec/adgytec-flow/utils/pointer"
@@ -30,24 +29,5 @@ func (e *RequestDecodeError) HTTPResponse() apires.ErrorDetails {
 	return apires.ErrorDetails{
 		HTTPStatusCode: e.Status,
 		Message:        pointer.New(e.Error()),
-	}
-}
-
-type RequestValidationError struct {
-	FieldErrors map[string]string
-}
-
-func (e *RequestValidationError) Error() string {
-	return "invalid request body"
-}
-
-func (e *RequestValidationError) Is(target error) bool {
-	return target == ErrRequestValidation
-}
-
-func (e *RequestValidationError) HTTPResponse() apires.ErrorDetails {
-	return apires.ErrorDetails{
-		HTTPStatusCode: http.StatusUnprocessableEntity,
-		FieldErrors:    &e.FieldErrors,
 	}
 }
