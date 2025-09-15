1d67d43f48e1707a10c5d8419f86be8343796637
rohan <rohanverma031@gmail.com>
2025-09-15 19:13:05 +0530

    feat: added update user profile data struct

--- services/user/update_user_profile.go
+++ services/user/update_user_profile.go
@@ -4,12 +4,44 @@ import (
 	"net/http"
 
 	"github.com/Adgytec/adgytec-flow/utils/actor"
+	"github.com/Adgytec/adgytec-flow/utils/core"
 	"github.com/Adgytec/adgytec-flow/utils/payload"
 	"github.com/go-chi/chi/v5"
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/go-ozzo/ozzo-validation/v4/is"
 	"github.com/google/uuid"
 )
 
+type updateUserProfileData struct {
+	Name           string
+	ProfilePicture string
+	About          string
+	DateOfBirth    string
+}
+
+func (userProfile updateUserProfileData) Validate() error {
+	validationErr := validation.ValidateStruct(&userProfile,
+		validation.Field(&userProfile.Name, validation.Required, validation.Length(3, 100)),
+		validation.Field(&userProfile.ProfilePicture, is.UUID),
+		validation.Field(&userProfile.About, validation.Length(1, 1024)),
+		validation.Field(&userProfile.DateOfBirth, validation.Required, validation.Date("2006-01-02")),
+	)
+
+	if validationErr != nil {
+		return &core.FieldValidationError{
+			ValidationErrors: validationErr,
+		}
+	}
+
+	return nil
+}
+
 func (m *userServiceMux) updateUserProfileUtil(w http.ResponseWriter, r *http.Request, userID uuid.UUID) {
+	userProfieDetails, payloadErr := payload.DecodeRequestBodyAndValidate[updateUserProfileData](w, r)
+	if payloadErr != nil {
+		payload.EncodeError(w, payloadErr)
+		return
+	}
 }
 
 func (m *userServiceMux) updateSelfProfile(w http.ResponseWriter, r *http.Request) {
