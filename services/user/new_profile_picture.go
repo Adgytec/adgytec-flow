39212abf9eaaffc4aee1210e451a422d6029a9ec
rohan <rohanverma031@gmail.com>
2025-09-15 09:28:34 +0530

    feat: added new profile picture util defination

--- services/user/new_profile_picture.go
+++ services/user/new_profile_picture.go
@@ -1,15 +1,34 @@
 package user
 
 import (
+	"context"
 	"net/http"
 
+	"github.com/Adgytec/adgytec-flow/services/media"
 	"github.com/Adgytec/adgytec-flow/utils/actor"
 	"github.com/Adgytec/adgytec-flow/utils/payload"
 	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
 )
 
+func (s *userService) newProfilePicture(ctx context.Context, userID uuid.UUID, profilePictureDetails media.NewMediaItemInput) (*media.NewMediaItemOutput, error) {
+	return nil, nil
+}
+
 func (m *userServiceMux) newProfilePictureUtil(w http.ResponseWriter, r *http.Request, userID uuid.UUID) {
+	profilePictureDetails, reqBodyErr := payload.DecodeRequestBodyAndValidate[media.NewMediaItemInput](w, r)
+	if reqBodyErr != nil {
+		payload.EncodeError(w, reqBodyErr)
+		return
+	}
+
+	profilePictureDetailsOutput, uploadReqErr := m.service.newProfilePicture(r.Context(), userID, profilePictureDetails)
+	if uploadReqErr != nil {
+		payload.EncodeError(w, uploadReqErr)
+		return
+	}
+
+	payload.EncodeJSON(w, http.StatusCreated, profilePictureDetailsOutput)
 }
 
 func (m *userServiceMux) newSelfProfilePicture(w http.ResponseWriter, r *http.Request) {
