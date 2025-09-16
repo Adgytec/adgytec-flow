1c7b3959aeedb98a4b7f070016ff0d554e298e5c
rohan <rohanverma031@gmail.com>
2025-09-16 09:18:36 +0530

    feat: seperated media service pc interface for better usage across services

--- services/media/service_pc.go
+++ services/media/service_pc.go
@@ -8,23 +8,29 @@ import (
 	"github.com/google/uuid"
 )
 
+// Contains methods that can be run without a transaction.
 type MediaServicePC interface {
 	NewMediaItem(ctx context.Context, input NewMediaItemInput) (NewMediaItemOutput, error)
 	NewMediaItems(ctx context.Context, input []NewMediaItemInput) ([]NewMediaItemOutput, error)
+}
+
+// Contains methods that MUST be run within a transaction.
+type TransactionalMediaServicePC interface {
 	CompleteMediaItemUpload(ctx context.Context, mediaID uuid.UUID) error
 	CompleteMediaItemsUpload(ctx context.Context, mediaIDs []uuid.UUID) error
 }
 
+// The main service interface that provides access to both.
 type MediaServicePCWithTransaction interface {
 	MediaServicePC
-	WithTransaction(db database.Database) MediaServicePC
+	WithTransaction(db database.Database) TransactionalMediaServicePC
 }
 
 type mediaServicePC struct {
 	service *mediaService
 }
 
-func (pc *mediaServicePC) WithTransaction(db database.Database) MediaServicePC {
+func (pc *mediaServicePC) WithTransaction(db database.Database) TransactionalMediaServicePC {
 	return &mediaServicePC{
 		service: &mediaService{
 			storage:  pc.service.storage,
