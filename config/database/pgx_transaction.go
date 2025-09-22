0b7ba3e5e5e829ccbe9a160341643bff323e227e
rohan <rohanverma031@gmail.com>
2025-09-22 20:22:26 +0530

    update: updated nested transaction behaviour

--- config/database/pgx_transaction.go
+++ config/database/pgx_transaction.go
@@ -7,6 +7,16 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
+type txStub struct{}
+
+func (tx txStub) Commit(_ context.Context) error {
+	return nil
+}
+
+func (tx txStub) Rollback(_ context.Context) error {
+	return nil
+}
+
 type pgxTx struct {
 	queries *db.Queries
 }
@@ -15,10 +25,10 @@ func (c *pgxTx) Queries() *db.Queries {
 	return c.queries
 }
 
-// WithTransaction prevents nested transactions by always returning ErrRequestingTransactionInsideTransaction.
-// Callers can check for this error to know they are already within a transaction.
+// WithTransaction() return parent transaction with stub for Tx interface
+// actual transaction methods will be handled by parent action
 func (c *pgxTx) WithTransaction(_ context.Context) (Database, Tx, error) {
-	return nil, nil, ErrRequestingTransactionInsideTransaction
+	return c, txStub{}, nil
 }
 
 func newPgxTx(conn pgx.Tx, queries *db.Queries) Database {
