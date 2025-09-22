29d264869f06a14e12c36bc92dbfb076f002db23
rohan <rohanverma031@gmail.com>
2025-09-22 11:49:17 +0530

    update: new media item input to use mime type to validate meida type for upload

--- services/media/types.go
+++ services/media/types.go
@@ -1,8 +1,10 @@
 package media
 
 import (
+	"fmt"
 	"path"
 	"path/filepath"
+	"strings"
 
 	"github.com/Adgytec/adgytec-flow/database/db"
 	"github.com/Adgytec/adgytec-flow/utils/core"
@@ -11,9 +13,9 @@ import (
 )
 
 type NewMediaItemInput struct {
-	Size      int64
-	MediaType db.GlobalMediaType
-	Name      string
+	Size     int64
+	Name     string
+	MimeType string
 }
 
 // Validate() validates the input values
@@ -22,22 +24,14 @@ func (mediaItemInput NewMediaItemInput) Validate() error {
 		validation.Field(
 			&mediaItemInput.Size,
 			validation.Required,
-			validation.Min(1),
-		),
-		validation.Field(
-			&mediaItemInput.MediaType,
-			validation.Required,
-			validation.By(
-				func(_ any) error {
-					if !mediaItemInput.MediaType.Valid() {
-						return ErrInvalidMediaTypeValue
-					}
-					return nil
-				},
-			),
+			validation.Min(0),
 		),
 		validation.Field(
 			&mediaItemInput.Name,
+			validation.Required,
+		),
+		validation.Field(
+			&mediaItemInput.MimeType,
 			validation.Required,
 		),
 	)
@@ -51,41 +45,25 @@ func (mediaItemInput NewMediaItemInput) Validate() error {
 	return nil
 }
 
-func (mediaItemInput NewMediaItemInput) ensureMediaTypeValue(value db.GlobalMediaType) error {
-	validationErr := mediaItemInput.Validate()
-	if validationErr != nil {
-		return validationErr
-	}
-
-	mediaTypeValidationErr := validation.ValidateStruct(
-		&mediaItemInput,
-		validation.Field(
-			&mediaItemInput.MediaType,
-			validation.In(
-				value,
-			),
-		),
-	)
-
-	if mediaTypeValidationErr != nil {
-		return &core.FieldValidationError{
-			ValidationErrors: mediaTypeValidationErr,
+func (mediaItemInput NewMediaItemInput) ensureMediaItemType(requiredType string) error {
+	if !strings.HasPrefix(mediaItemInput.MimeType, requiredType) {
+		return &InvalidMediaTypeValueError{
+			Required: fmt.Sprintf("%s/*", requiredType),
+			Got:      mediaItemInput.MimeType,
 		}
 	}
 
 	return nil
-
 }
 
 // EnsureMediaItemIsImage() ensures the item that will be uploaded is image
-// this just validated mediaType value later when CompleteMediaItemUpload() is called than the uploaded file bytes are checked for the acutal media type
 func (mediaItemInput NewMediaItemInput) EnsureMediaItemIsImage() error {
-	return mediaItemInput.ensureMediaTypeValue(db.GlobalMediaTypeImage)
+	return mediaItemInput.ensureMediaItemType("image")
 }
 
 // EnsureMediaItemIsVideo() ensures the item that will be uploaded is video
 func (mediaItemInput NewMediaItemInput) EnsureMediaItemIsVideo() error {
-	return mediaItemInput.ensureMediaTypeValue(db.GlobalMediaTypeVideo)
+	return mediaItemInput.ensureMediaItemType("video")
 }
 
 func (mediaItemInput NewMediaItemInput) getMediaItemExtension() string {
