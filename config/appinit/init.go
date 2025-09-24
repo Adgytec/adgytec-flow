f13026538a99f44e8d335610c84f55d3b8bbfb18
rohan <rohanverma031@gmail.com>
2025-09-24 19:06:27 +0530

    feat: updated service initializtion to be handled in appinit package

--- config/appinit/init.go
+++ config/appinit/init.go
@@ -1,32 +1,46 @@
 package appinit
 
 import (
+	"context"
 	"log"
 
 	"github.com/Adgytec/adgytec-flow/config/app"
+	"github.com/Adgytec/adgytec-flow/database/db"
 	"github.com/Adgytec/adgytec-flow/services/iam"
 	"github.com/Adgytec/adgytec-flow/services/user"
-	"github.com/Adgytec/adgytec-flow/utils/services"
+	"github.com/Adgytec/adgytec-flow/utils/core"
 )
 
-type serviceFactory func(params app.App) services.Init
+type serviceFactory func() (db.AddServiceDetailsParams, []db.AddManagementPermissionParams, []db.AddApplicationPermissionParams)
 
 var appServices = []serviceFactory{
-	func(appConfig app.App) services.Init {
-		return iam.InitIAMService(appConfig)
-	},
-	func(appConfig app.App) services.Init {
-		return user.InitUserService(appConfig)
-	},
+	iam.InitIAMService,
+	user.InitUserService,
 }
 
 func EnsureServicesInitialization(appConfig app.App) {
 	log.Println("Ensuring application initialization.")
 	for _, factory := range appServices {
-		serviceInit := factory(appConfig)
-		if err := serviceInit.InitService(); err != nil {
-			log.Fatalf("error initializaing services. Can't proceed without resolving: %v", err)
+		details, managementPermissions, applicationPermissions := factory()
+
+		if err := appConfig.Database().Queries().AddServiceDetails(context.TODO(), details); err != nil {
+			log.Fatal(err)
 		}
+
+		for _, perm := range managementPermissions {
+			perm.ID = core.GetIDFromPayload([]byte(perm.Key))
+			if err := appConfig.Database().Queries().AddManagementPermission(context.TODO(), perm); err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		for _, perm := range applicationPermissions {
+			perm.ID = core.GetIDFromPayload([]byte(perm.Key))
+			if err := appConfig.Database().Queries().AddApplicationPermission(context.TODO(), perm); err != nil {
+				log.Fatal(err)
+			}
+		}
+
 	}
 
 }
