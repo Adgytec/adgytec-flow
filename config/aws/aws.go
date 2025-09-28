8161c665a41894e5cb454ee99b3662b8f5e324e4
rohan <rohanverma031@gmail.com>
2025-09-28 21:39:14 +0530

    feat: added parent error for aws config

--- config/aws/aws.go
+++ config/aws/aws.go
@@ -12,7 +12,9 @@ func NewAWSConfig() (aws.Config, error) {
 	log.Println("loading aws config")
 	cfg, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
-		return aws.Config{}, ErrInvalidAWSConfig
+		return aws.Config{}, &InvalidAWSConfigError{
+			cause: err,
+		}
 	}
 
 	return cfg, nil
--- config/aws/errors.go
+++ config/aws/errors.go
@@ -1,7 +1,11 @@
 package aws
 
-import "errors"
+import "fmt"
 
-var (
-	ErrInvalidAWSConfig = errors.New("invalid aws config")
-)
+type InvalidAWSConfigError struct {
+	cause error
+}
+
+func (e *InvalidAWSConfigError) Error() string {
+	return fmt.Sprintf("error loading AWS default config: %w", e.cause)
+}
