30b111bc56c6c9e73bee6fee2f73390563135f2a
rohan <rohanverma031@gmail.com>
2025-09-28 18:21:22 +0530

    feat: added invalid api key error

---
+++ utils/api_key/errors.go
@@ -0,0 +1,33 @@
+package apikey
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/Adgytec/adgytec-flow/utils/apires"
+	"github.com/Adgytec/adgytec-flow/utils/pointer"
+)
+
+var (
+	ErrInvalidApiKey = errors.New("invalid-api-key")
+)
+
+type InvalidApiKeyError struct {
+	apiKey string
+}
+
+func (e *InvalidApiKeyError) Error() string {
+	return fmt.Sprintf("got invalid api key value: %s", e.apiKey)
+}
+
+func (e *InvalidApiKeyError) Is(target error) bool {
+	return target == ErrInvalidApiKey
+}
+
+func (e *InvalidApiKeyError) HTTPResponse() apires.ErrorDetails {
+	return apires.ErrorDetails{
+		HTTPStatusCode: http.StatusBadRequest,
+		Message:        pointer.New(ErrInvalidApiKey.Error()),
+	}
+}
