7d405fd94eead7152afa448c92be1e5adffddc03
rohan <rohanverma031@gmail.com>
2025-09-28 10:25:47 +0530

    feat: added auth new user impl

--- config/auth/new_user.go
+++ config/auth/new_user.go
@@ -1,7 +1,37 @@
 package auth
 
-import "context"
+import (
+	"context"
+	"errors"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider"
+	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider/types"
+)
 
 func (a *authCognito) NewUser(ctx context.Context, username string) error {
+	_, newUserErr := a.client.AdminCreateUser(
+		ctx,
+		&cognitoidentityprovider.AdminCreateUserInput{
+			UserPoolId: aws.String(a.userPoolID),
+			Username:   aws.String(username),
+		},
+	)
+	if newUserErr != nil {
+		var userExistsError *types.UsernameExistsException
+		if errors.As(newUserErr, &userExistsError) {
+			return &UserExistsError{
+				username: username,
+			}
+		}
+
+		return &AuthActionFailedError{
+			username:   username,
+			cause:      newUserErr,
+			reason:     newUserErr.Error(),
+			actionType: authActionTypeCreate,
+		}
+	}
+
 	return nil
 }
