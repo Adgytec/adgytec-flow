ff4b51dcce77bc07d22dce082b1f624b6cec4c6e
rohan <rohanverma031@gmail.com>
2025-09-29 10:17:25 +0530

    feat: added impl for new presign upload part method

--- config/storage/new_presign_upload_part.go
+++ config/storage/new_presign_upload_part.go
@@ -3,9 +3,26 @@ package storage
 import (
 	"context"
 
-	"github.com/Adgytec/adgytec-flow/utils/core"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 func (s *s3Client) NewPresignUploadPart(ctx context.Context, key, uploadID string, partNumber int32) (string, error) {
-	return "", core.ErrNotImplemented
+	presignHTTPReq, presingErr := s.presignClient.PresignUploadPart(
+		ctx,
+		&s3.UploadPartInput{
+			Bucket:     aws.String(s.bucket),
+			Key:        aws.String(key),
+			PartNumber: aws.Int32(partNumber),
+			UploadId:   aws.String(uploadID),
+		},
+		func(po *s3.PresignOptions) {
+			po.Expires = s.presignExpiration
+		},
+	)
+	if presingErr != nil {
+		return "", presingErr
+	}
+
+	return presignHTTPReq.URL, nil
 }
