9238d9100f25ee3350c13005df3771195ab81c60
rohan <rohanverma031@gmail.com>
2025-09-29 19:27:26 +0530

    feat: added parts info detail to complete multipart upload

--- config/storage/complete_multipart_upload.go
+++ config/storage/complete_multipart_upload.go
@@ -5,15 +5,17 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 )
 
-func (s *s3Client) CompleteMultipartUpload(ctx context.Context, key, uploadID string) error {
+func (s *s3Client) CompleteMultipartUpload(ctx context.Context, key, uploadID string, partsInfo types.CompletedMultipartUpload) error {
 	_, completeUploadErr := s.client.CompleteMultipartUpload(
 		ctx,
 		&s3.CompleteMultipartUploadInput{
-			Bucket:   aws.String(s.bucket),
-			Key:      aws.String(key),
-			UploadId: aws.String(uploadID),
+			Bucket:          aws.String(s.bucket),
+			Key:             aws.String(key),
+			UploadId:        aws.String(uploadID),
+			MultipartUpload: &partsInfo,
 		},
 	)
 	return completeUploadErr
--- config/storage/storage.go
+++ config/storage/storage.go
@@ -3,6 +3,8 @@ package storage
 import (
 	"context"
 	"time"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 )
 
 const tempObjectTag = "status=temp"
@@ -12,6 +14,6 @@ type Storage interface {
 	NewPresignPut(ctx context.Context, key string) (string, error)
 	NewMultipartUpload(ctx context.Context, key string) (string, error)
 	NewPresignUploadPart(ctx context.Context, key, uploadID string, partNumber int32) (string, error)
-	CompleteMultipartUpload(ctx context.Context, key, uploadID string) error
+	CompleteMultipartUpload(ctx context.Context, key, uploadID string, partsInfo types.CompletedMultipartUpload) error
 	DeleteObjectTempTag(ctx context.Context, key string) error
 }
