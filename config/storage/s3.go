ea99d53b9ef4e3e9945783420723b036d8e27826
rohan <rohanverma031@gmail.com>
2025-09-29 09:48:20 +0530

    feat: added storage error for invalid config

--- config/storage/s3.go
+++ config/storage/s3.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"log"
+	"os"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -10,14 +11,25 @@ import (
 type s3Client struct {
 	client        *s3.Client
 	presignClient *s3.PresignClient
+	bucket        string
+	bucketRegion  string
 }
 
-func NewS3Client(awsConfig aws.Config) Storage {
+func NewS3Client(awsConfig aws.Config) (Storage, error) {
 	log.Println("creating s3 client")
+
+	bucket := os.Getenv("AWS_S3_STUDIO_BUCKET")
+	bucketRegion := os.Getenv("AWS_S3_STUDIO_BUCKET_REGION")
+
+	if bucket == "" || bucketRegion == "" {
+		return nil, ErrInvalidS3ConfigValue
+	}
 
 	client := s3.NewFromConfig(awsConfig)
 	return &s3Client{
 		client:        client,
 		presignClient: s3.NewPresignClient(client),
-	}
+		bucket:        bucket,
+		bucketRegion:  bucketRegion,
+	}, nil
 }
