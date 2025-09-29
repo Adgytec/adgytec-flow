93813a71a91b2ec883c89b6b28801057e601fd63
rohan <rohanverma031@gmail.com>
2025-09-29 11:24:08 +0530

    update: removed unnecessary bucket region value from s3client struct

--- config/storage/s3.go
+++ config/storage/s3.go
@@ -12,16 +12,14 @@ type s3Client struct {
 	client        *s3.Client
 	presignClient *s3.PresignClient
 	bucket        string
-	bucketRegion  string
 }
 
 func NewS3Client(awsConfig aws.Config) (Storage, error) {
 	log.Println("creating s3 client")
 
 	bucket := os.Getenv("AWS_S3_STUDIO_BUCKET")
-	bucketRegion := os.Getenv("AWS_S3_STUDIO_BUCKET_REGION")
 
-	if bucket == "" || bucketRegion == "" {
+	if bucket == "" {
 		return nil, ErrInvalidS3ConfigValue
 	}
 
@@ -30,6 +28,5 @@ func NewS3Client(awsConfig aws.Config) (Storage, error) {
 		client:        client,
 		presignClient: s3.NewPresignClient(client),
 		bucket:        bucket,
-		bucketRegion:  bucketRegion,
 	}, nil
 }
