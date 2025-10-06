111ab849172586667dcae4ebfd1dc942df0565e5
rohan <rohanverma031@gmail.com>
2025-10-06 19:30:05 +0530

    update: updated new presign put and new multipart upload method to include id and use new object tag method

--- config/storage/new_presign_put.go
+++ config/storage/new_presign_put.go
@@ -6,15 +6,16 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/google/uuid"
 )
 
-func (s *s3Client) NewPresignPut(ctx context.Context, key string) (string, error) {
+func (s *s3Client) NewPresignPut(ctx context.Context, key string, id uuid.UUID) (string, error) {
 	presignHTTPReq, presignErr := s.presignClient.PresignPutObject(
 		ctx,
 		&s3.PutObjectInput{
 			Bucket:  aws.String(s.bucket),
 			Key:     aws.String(key),
-			Tagging: aws.String(tempObjectTag),
+			Tagging: aws.String(newObjectTag(id)),
 		},
 		func(po *s3.PresignOptions) {
 			po.Expires = presignExpiration
