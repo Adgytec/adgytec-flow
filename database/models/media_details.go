05a1b8fb18f1963625f5deabde43b1cc7fb8ef38
rohan <rohanverma031@gmail.com>
2025-10-08 19:25:08 +0530

    update: added media id in image and video details model

--- database/models/media_details.go
+++ database/models/media_details.go
@@ -1,21 +1,25 @@
 package models
 
+import "github.com/google/uuid"
+
 type ImageDetails struct {
-	OriginalImage *string `json:"originalImage"`
-	Size          *int64  `json:"size"`
-	Status        *string `json:"status"`
-	Thumbnail     *string `json:"thumbnail"`
-	Small         *string `json:"small"`
-	Medium        *string `json:"medium"`
-	Large         *string `json:"large"`
-	ExtraLarge    *string `json:"extraLarge"`
+	MediaID       uuid.UUID `json:"-"` // used for internal purposes
+	OriginalImage *string   `json:"originalImage"`
+	Size          *int64    `json:"size"`
+	Status        *string   `json:"status"`
+	Thumbnail     *string   `json:"thumbnail"`
+	Small         *string   `json:"small"`
+	Medium        *string   `json:"medium"`
+	Large         *string   `json:"large"`
+	ExtraLarge    *string   `json:"extraLarge"`
 }
 
 type VideoDetails struct {
-	OriginalVideo    *string `json:"originalVideo"`
-	Size             *int64  `json:"size"`
-	Status           *string `json:"status"`
-	Thumbnail        *string `json:"thumbnail"`
-	AdaptiveManifest *string `json:"adaptiveManifest"`
-	Preview          *string `json:"preview"`
+	MediaID          uuid.UUID `json:"-"` // used for internal purposes
+	OriginalVideo    *string   `json:"originalVideo"`
+	Size             *int64    `json:"size"`
+	Status           *string   `json:"status"`
+	Thumbnail        *string   `json:"thumbnail"`
+	AdaptiveManifest *string   `json:"adaptiveManifest"`
+	Preview          *string   `json:"preview"`
 }
