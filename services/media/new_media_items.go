3b1a9231006c3959a699c14278e323064ca714e9
rohan <rohanverma031@gmail.com>
2025-10-09 12:52:50 +0530

    feat: added mediaServiceActions interface impl

--- services/media/new_media_items.go
+++ services/media/new_media_items.go
@@ -1,1 +1,24 @@
 package media
+
+import "context"
+
+func (s *mediaService) newMediaItems(ctx context.Context, input []NewMediaItemInfoWithStorageDetails) ([]MediaUploadDetails, error) {
+	return nil, nil
+}
+
+func (a *mediaServiceActions) NewMediaItem(ctx context.Context, input NewMediaItemInfoWithStorageDetails) (*MediaUploadDetails, error) {
+	uploadDetails, newMediaErr := a.service.newMediaItems(ctx, []NewMediaItemInfoWithStorageDetails{input})
+	if newMediaErr != nil {
+		return nil, newMediaErr
+	}
+
+	if len(uploadDetails) != 1 {
+		return nil, ErrCreatingNewMediaItem
+	}
+
+	return &uploadDetails[0], nil
+}
+
+func (a *mediaServiceActions) NewMediaItems(ctx context.Context, input []NewMediaItemInfoWithStorageDetails) ([]MediaUploadDetails, error) {
+	return a.service.newMediaItems(ctx, input)
+}
