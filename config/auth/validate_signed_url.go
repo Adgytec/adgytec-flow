1e20db0725ce2d47139e909514756829c03bd749
rohan <rohanverma031@gmail.com>
2025-10-09 19:58:29 +0530

    feat: added method impl for validating signed urls

--- config/auth/validate_signed_url.go
+++ config/auth/validate_signed_url.go
@@ -3,11 +3,81 @@ package auth
 import (
 	"context"
 	"net/url"
+	"sort"
+	"strconv"
+	"time"
 
 	"github.com/Adgytec/adgytec-flow/utils/actor"
 )
 
+func validateExpiry(epochString string) error {
+	epochInt, parseErr := strconv.ParseInt(epochString, 10, 64)
+	if parseErr != nil {
+		return &InvalidSignedURLError{}
+	}
+
+	expiry := time.Unix(epochInt, 0)
+	if time.Now().After(expiry) {
+		return &SignedURLExpiredError{}
+	}
+
+	return nil
+}
+
+// validateSignedURL() baseQuery contains extra query params that are not directly included in url
 func (a *authCommon) validateSignedURL(signedURL url.URL, baseQuery map[string]string) error {
+	queryParams := signedURL.Query()
+
+	hashSignatureSlice := queryParams[queryKeySignature]
+	expireSlice := queryParams[queryKeyExpire]
+	if len(hashSignatureSlice) != 1 && len(expireSlice) != 1 {
+		return &InvalidSignedURLError{}
+	}
+
+	// remove signature from query params
+	delete(queryParams, queryKeySignature)
+
+	hashSignature := hashSignatureSlice[0]
+	expireString := expireSlice[0]
+
+	// validate expiry
+	expireErr := validateExpiry(expireString)
+	if expireErr != nil {
+		return expireErr
+	}
+
+	// create custom query
+	query := make(map[string]string)
+
+	// add query params to query
+	for key, value := range queryParams {
+		// only consider first element
+		query[key] = value[0]
+	}
+
+	// add base query to query
+	for key, value := range baseQuery {
+		query[key] = value
+	}
+
+	// sort query keys
+	queryKeys := make([]string, 0, len(query))
+	for key, _ := range query {
+		queryKeys = append(queryKeys, key)
+	}
+	sort.Strings(queryKeys)
+
+	hashPayload := make([]byte, 0)
+	for _, key := range queryKeys {
+		hashPayload = append(hashPayload, []byte(query[key])...)
+	}
+
+	// compare hash
+	compareErr := a.compareSignedHash(hashSignature, []byte(signedURL.Path), hashPayload)
+	if compareErr != nil {
+		return compareErr
+	}
+
 	return nil
 }
 
