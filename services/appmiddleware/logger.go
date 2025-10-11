1235ec457f5876bfd399d772e37ece53a7394251
rohan <rohanverma031@gmail.com>
2025-10-11 14:39:05 +0530

    feat: added logger middleware implementation

---
+++ services/appmiddleware/logger.go
@@ -0,0 +1,45 @@
+package appmiddleware
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/Adgytec/adgytec-flow/utils/logger"
+	"github.com/google/uuid"
+	"github.com/rs/zerolog/log"
+)
+
+func (pc *appMiddlewarePC) Logger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		// create or get request id
+		reqID := r.Header.Get("X-Request-ID")
+		if reqID == "" {
+			reqID = uuid.New().String()
+		}
+
+		// wrap response writer to capture status code
+		rw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+
+		// create logger with context
+		reqLogger := log.With().Str("request_id", reqID).Logger()
+		reqCtxWithLogger := logger.AddLoggerToContext(&reqLogger, r.Context())
+		r = r.WithContext(reqCtxWithLogger)
+
+		// log request details
+		defer func() {
+			reqLogger.Info().
+				Str("method", r.Method).
+				Str("url", r.URL.RequestURI()).
+				Str("path", r.URL.Path).
+				Int("status", rw.statusCode).
+				Str("user_agent", r.UserAgent()).
+				Str("remote_ip", r.RemoteAddr).
+				Dur("elapsed_ms", time.Since(start)).
+				Msg("incoming request")
+		}()
+
+		next.ServeHTTP(rw, r)
+	})
+}
