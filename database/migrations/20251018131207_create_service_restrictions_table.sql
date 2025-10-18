4c4721931ef891cee0a562f20ac71532ca18a83a
rohan <rohanverma031@gmail.com>
2025-10-18 18:53:39 +0530

    update: make service restriction name unique

--- database/migrations/20251018131207_create_service_restrictions_table.sql
+++ database/migrations/20251018131207_create_service_restrictions_table.sql
@@ -7,7 +7,7 @@ this table also helps during organization create for defining organization plan 
 CREATE TABLE IF NOT EXISTS global.service_restrictions (
 	id UUID PRIMARY KEY,
 	service_id UUID NOT NULL REFERENCES global.services (id) ON DELETE CASCADE,
-	name TEXT NOT NULL,
+	name TEXT NOT NULL UNIQUE,
 	description TEXT,
 	value_type TEXT NOT NULL
 );
