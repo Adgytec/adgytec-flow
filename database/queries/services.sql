e16e568100e90dbbd86819d689d2c29f5d7155cb
rohan <rohanverma031@gmail.com>
2025-10-18 11:37:04 +0530

    feat: added copyfrom usage with upsert insert for services data

--- database/queries/services.sql
+++ database/queries/services.sql
@@ -1,8 +1,25 @@
--- name: AddServiceDetails :exec
+-- name: NewServiceStagingTable :exec
+CREATE TEMP TABLE services_staging (
+	LIKE global.services including ALL
+) ON
+COMMIT delete rows;
+
+-- name: AddServicesIntoStaging :copyfrom
 INSERT INTO
-	global.services (id, name, type)
+	services_staging (id, name, type)
 VALUES
-	($1, $2, $3)
+	($1, $2, $3);
+
+-- name: UpsertServicesFromStaging :exec
+INSERT INTO
+	global.services AS t (id, name, type)
+SELECT
+	id,
+	name,
+	type
+FROM
+	services_staging
 ON CONFLICT (id) DO UPDATE
 SET
-	type = excluded.type;
+	type = excluded.type
+    where t.type is distinct from excluded.type;
