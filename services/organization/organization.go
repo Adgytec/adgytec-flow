79d7600a69a02d3e0905716f8809188d6a5400d6
rohan <rohanverma031@gmail.com>
2025-10-25 21:55:28 +0530

    update: added service details to org service and removed core service restrictions cache client

--- services/organization/organization.go
+++ services/organization/organization.go
@@ -3,12 +3,11 @@ package org
 import (
 	"github.com/Adgytec/adgytec-flow/config/cache"
 	"github.com/Adgytec/adgytec-flow/config/database"
-	"github.com/Adgytec/adgytec-flow/config/serializer"
-	"github.com/Adgytec/adgytec-flow/database/db"
 	"github.com/Adgytec/adgytec-flow/services/iam"
 	"github.com/Adgytec/adgytec-flow/services/media"
 	"github.com/Adgytec/adgytec-flow/services/user"
 	"github.com/Adgytec/adgytec-flow/utils/core"
+	"github.com/Adgytec/adgytec-flow/utils/staging"
 )
 
 type orgServiceParams interface {
@@ -17,6 +16,7 @@ type orgServiceParams interface {
 	CacheClient() cache.CacheClient
 	MediaWithTransaction() media.MediaServicePC
 	IAMService() iam.IAMServicePC
+	Services() staging.Services
 }
 
 type orgServiceMuxParams interface {
@@ -25,23 +25,19 @@ type orgServiceMuxParams interface {
 }
 
 type orgService struct {
-	db                           database.Database
-	userService                  user.UserServicePC
-	media                        media.MediaServicePC
-	iam                          iam.IAMServicePC
-	coreServiceRestrictionsCache cache.Cache[[]db.GetCoreServiceRestrictionsRow]
+	db             database.Database
+	userService    user.UserServicePC
+	media          media.MediaServicePC
+	iam            iam.IAMServicePC
+	serviceDetails staging.Services
 }
 
 func newOrgService(params orgServiceParams) *orgService {
 	return &orgService{
-		db:          params.Database(),
-		userService: params.UserService(),
-		media:       params.MediaWithTransaction(),
-		iam:         params.IAMService(),
-		coreServiceRestrictionsCache: cache.NewCache(
-			params.CacheClient(),
-			serializer.NewGobSerializer[[]db.GetCoreServiceRestrictionsRow](),
-			"core-service-restriction",
-		),
+		db:             params.Database(),
+		userService:    params.UserService(),
+		media:          params.MediaWithTransaction(),
+		iam:            params.IAMService(),
+		serviceDetails: params.Services(),
 	}
 }
