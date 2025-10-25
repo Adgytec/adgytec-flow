3646a981844970901524c94e073d7b674ef912c1
rohan <rohanverma031@gmail.com>
2025-10-25 21:40:17 +0530

    feat: added management permission for organization

--- services/organization/service_init.go
+++ services/organization/service_init.go
@@ -5,7 +5,7 @@ import "github.com/Adgytec/adgytec-flow/utils/staging"
 func InitOrgService() staging.Details {
 	return staging.Details{
 		Service:                orgServiceDetails,
-		ManagementPermissions:  nil,
+		ManagementPermissions:  managementPermissions,
 		ApplicationPermissions: nil,
 		ServiceRestrictions:    nil,
 	}
