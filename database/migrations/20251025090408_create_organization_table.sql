3ffb76ee776a7306fc3f590ad8e89cfcfd46e3c7
rohan <rohanverma031@gmail.com>
2025-10-26 02:08:42 +0530

    fix: added missing trigger for created at value for organization table

--- database/migrations/20251025090408_create_organization_table.sql
+++ database/migrations/20251025090408_create_organization_table.sql
@@ -10,9 +10,22 @@ CREATE TABLE IF NOT EXISTS global.organizations (
 	created_at TIMESTAMPTZ NOT NULL
 );
 
+CREATE OR REPLACE TRIGGER on_insert_set_created_at before insert ON global.organizations FOR each ROW
+EXECUTE function global.set_created_at ();
+
+CREATE OR REPLACE TRIGGER on_update_prevent_created_at_update before
+UPDATE ON global.organizations FOR each ROW WHEN (
+	old.created_at IS DISTINCT FROM new.created_at
+)
+EXECUTE function global.created_at_update ();
+
 -- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
+DROP TRIGGER if EXISTS on_update_prevent_created_at_update ON global.organizations;
+
+DROP TRIGGER if EXISTS on_insert_set_created_at ON global.organizations;
+
 DROP TABLE global.organizations;
 
 -- +goose StatementEnd
