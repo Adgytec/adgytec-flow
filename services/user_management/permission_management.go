2d9a20526b0e2b296ec97dcbf5100024a9f96627
rohan <rohanverma031@gmail.com>
2025-10-27 16:44:28 +0530

    feat: add permission details for user management service

--- services/user_management/permission_management.go
+++ services/user_management/permission_management.go
@@ -1,6 +1,13 @@
 package usermanagement
 
-import "github.com/Adgytec/adgytec-flow/database/db"
+import (
+	"fmt"
+
+	"github.com/Adgytec/adgytec-flow/database/db"
+	"github.com/Adgytec/adgytec-flow/utils/markdown"
+
+	md "github.com/nao1215/markdown"
+)
 
 var managementPermissions = []db.AddManagementPermissionsIntoStagingParams{
 	newManagementUserPermission,
@@ -11,14 +18,74 @@ var managementPermissions = []db.AddManagementPermissionsIntoStagingParams{
 	removeUserFromUserGroupPermission,
 }
 
-var newManagementUserPermission = db.AddManagementPermissionsIntoStagingParams{}
+var newManagementUserPermission = db.AddManagementPermissionsIntoStagingParams{
+	Key:       fmt.Sprintf("%s:add:user", serviceDetails.Name),
+	ServiceID: serviceDetails.ID,
+	Name:      "Add User",
+	Description: markdown.BuildMarkdown(func(m *md.Markdown) {
+		m.H3("Add User").
+			PlainText("Grants the ability to add user for management purposes.")
+	}),
+	RequiredResources: []string{},
+	AssignableActor:   db.GlobalAssignableActorTypeUser,
+}
 
-var newUserGroupPermission = db.AddManagementPermissionsIntoStagingParams{}
+var newUserGroupPermission = db.AddManagementPermissionsIntoStagingParams{
+	Key:       fmt.Sprintf("%s:create:user-group", serviceDetails.Name),
+	ServiceID: serviceDetails.ID,
+	Name:      "Create User Group",
+	Description: markdown.BuildMarkdown(func(m *md.Markdown) {
+		m.H3("Create User Group").
+			PlainText("Grants the ability to create user groups.")
+	}),
+	RequiredResources: []string{},
+	AssignableActor:   db.GlobalAssignableActorTypeUser,
+}
 
-var updateUserGroupPermission = db.AddManagementPermissionsIntoStagingParams{}
+var updateUserGroupPermission = db.AddManagementPermissionsIntoStagingParams{
+	Key:       fmt.Sprintf("%s:update:user-group", serviceDetails.Name),
+	ServiceID: serviceDetails.ID,
+	Name:      "Update User Group",
+	Description: markdown.BuildMarkdown(func(m *md.Markdown) {
+		m.H3("Update User Group").
+			PlainText("Grants the ability to update user group details.")
+	}),
+	RequiredResources: []string{},
+	AssignableActor:   db.GlobalAssignableActorTypeUser,
+}
 
-var deleteUserGroupPermission = db.AddManagementPermissionsIntoStagingParams{}
+var deleteUserGroupPermission = db.AddManagementPermissionsIntoStagingParams{
+	Key:       fmt.Sprintf("%s:delete:user-group", serviceDetails.Name),
+	ServiceID: serviceDetails.ID,
+	Name:      "Delete User Group",
+	Description: markdown.BuildMarkdown(func(m *md.Markdown) {
+		m.H3("Delete User Group").
+			PlainText("Grants the ability to delete user group.")
+	}),
+	RequiredResources: []string{},
+	AssignableActor:   db.GlobalAssignableActorTypeUser,
+}
 
-var addUserInUserGroupPermission = db.AddManagementPermissionsIntoStagingParams{}
+var addUserInUserGroupPermission = db.AddManagementPermissionsIntoStagingParams{
+	Key:       fmt.Sprintf("%s:add:user-group-user", serviceDetails.Name),
+	ServiceID: serviceDetails.ID,
+	Name:      "Add User To User Group",
+	Description: markdown.BuildMarkdown(func(m *md.Markdown) {
+		m.H3("Add User To User Group").
+			PlainText("Grants the ability to add user to a user group.")
+	}),
+	RequiredResources: []string{},
+	AssignableActor:   db.GlobalAssignableActorTypeUser,
+}
 
-var removeUserFromUserGroupPermission = db.AddManagementPermissionsIntoStagingParams{}
+var removeUserFromUserGroupPermission = db.AddManagementPermissionsIntoStagingParams{
+	Key:       fmt.Sprintf("%s:remove:user-group-user", serviceDetails.Name),
+	ServiceID: serviceDetails.ID,
+	Name:      "Remove User From User Group",
+	Description: markdown.BuildMarkdown(func(m *md.Markdown) {
+		m.H3("Remove User From User Group").
+			PlainText("Grants the ability to remove user from a user group.")
+	}),
+	RequiredResources: []string{},
+	AssignableActor:   db.GlobalAssignableActorTypeUser,
+}
