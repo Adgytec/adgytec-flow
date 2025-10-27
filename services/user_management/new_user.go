7cce4f394cd77eb75d257d437e0c2fe66591b8a5
rohan <rohanverma031@gmail.com>
2025-10-27 19:01:17 +0530

    feat: added route to add new management user

---
+++ services/user_management/new_user.go
@@ -0,0 +1,12 @@
+package usermanagement
+
+import (
+	"context"
+	"net/http"
+)
+
+func (s *userManagementService) newManagementUser(ctx context.Context) error {
+	return nil
+}
+
+func (m *serviceMux) newManagementUser(w http.ResponseWriter, r *http.Request) {}
