619b76f1ab86ef14153c89f8e3ea55f726bd95c3
rohan <rohanverma031@gmail.com>
2025-10-28 16:46:15 +0530

    feat: added indexes for users table normalized name and email for search query

---
+++ database/migrations/20251028111408_global_user_table_query_search_index.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE INDEX if NOT EXISTS idx_users_normalized_name ON global.users (normalized_name);
+
+CREATE INDEX if NOT EXISTS idx_users_normalized_email ON global.users (normalized_email);
+
+-- +goose StatementEnd
+-- +goose Down
+-- +goose StatementBegin
+DROP INDEX if EXISTS idx_users_normalized_email ON global.users;
+
+DROP INDEX if EXISTS idx_users_normalized_name ON global.users;
+
+-- +goose StatementEnd
