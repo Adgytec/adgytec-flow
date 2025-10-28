fb0ea481c7142dae81455c5002b0e6263f6e04c2
rohan <rohanverma031@gmail.com>
2025-10-28 13:31:16 +0530

    feat: added impl for disable user

--- config/auth/disable_user.go
+++ config/auth/disable_user.go
@@ -1,7 +1,36 @@
 package auth
 
-import "context"
+import (
+	"context"
+	"errors"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider"
+	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider/types"
+)
 
 func (a *authCognito) DisableUser(ctx context.Context, username string) error {
+	_, disableUserErr := a.client.AdminDisableUser(ctx,
+		&cognitoidentityprovider.AdminDisableUserInput{
+			UserPoolId: aws.String(a.userPoolID),
+			Username:   aws.String(username),
+		},
+	)
+
+	if disableUserErr != nil {
+		var userExistsError *types.UsernameExistsException
+		if errors.As(disableUserErr, &userExistsError) {
+			return &UserExistsError{
+				username: username,
+			}
+		}
+
+		return &AuthActionFailedError{
+			username:   username,
+			cause:      disableUserErr,
+			actionType: authActionDisableUser,
+		}
+	}
+
 	return nil
 }
