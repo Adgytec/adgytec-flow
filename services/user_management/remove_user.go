156918fcc258fe335ed9da153dedbe0781b160d0
rohan <rohanverma031@gmail.com>
2025-10-28 11:23:40 +0530

    feat: added method impl for removing user from management

--- services/user_management/remove_user.go
+++ services/user_management/remove_user.go
@@ -4,11 +4,33 @@ import (
 	"context"
 	"net/http"
 
+	"github.com/Adgytec/adgytec-flow/services/iam"
 	"github.com/google/uuid"
 )
 
 func (s *userManagementService) removeUser(ctx context.Context, userID uuid.UUID) error {
-	return nil
+	permissionErr := s.iam.CheckPermission(ctx,
+		iam.NewPermissionRequiredFromManagementPermission(
+			removeManagementUserPermission,
+			iam.PermissionRequiredResources{},
+		),
+	)
+	if permissionErr != nil {
+		return permissionErr
+	}
+
+	qtx, tx, txErr := s.db.WithTransaction(ctx)
+	if txErr != nil {
+		return txErr
+	}
+	defer tx.Rollback(context.Background())
+
+	dbErr := qtx.Queries().RemoveManagementUser(ctx, userID)
+	if dbErr != nil {
+		return dbErr
+	}
+
+	return tx.Commit(ctx)
 }
 
 func (m *serviceMux) removeUser(w http.ResponseWriter, r *http.Request) {}
