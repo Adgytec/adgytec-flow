b29f799e1a8da4aa6b640bd4bbb84232dbe741a7
rohan <rohanverma031@gmail.com>
2025-10-28 19:19:26 +0530

    update: added text_pattern_ops to user name and email index for byte ordering

--- database/migrations/20251028111408_global_user_table_query_search_index.sql
+++ database/migrations/20251028111408_global_user_table_query_search_index.sql
@@ -1,8 +1,12 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE INDEX if NOT EXISTS idx_users_normalized_name ON global.users (normalized_name);
+CREATE INDEX if NOT EXISTS idx_users_normalized_name ON global.users (
+	normalized_name text_pattern_ops
+);
 
-CREATE INDEX if NOT EXISTS idx_users_normalized_email ON global.users (normalized_email);
+CREATE INDEX if NOT EXISTS idx_users_normalized_email ON global.users (
+	normalized_email text_pattern_ops
+);
 
 -- +goose StatementEnd
 -- +goose Down
