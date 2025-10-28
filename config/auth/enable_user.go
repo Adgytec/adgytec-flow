98e3f6c5ac01990f5bf8f967baedb0fc2e66409f
rohan <rohanverma031@gmail.com>
2025-10-28 13:32:28 +0530

    feat: added impl for enable user

--- config/auth/enable_user.go
+++ config/auth/enable_user.go
@@ -1,7 +1,36 @@
 package auth
 
-import "context"
+import (
+	"context"
+	"errors"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider"
+	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider/types"
+)
 
 func (a *authCognito) EnableUser(ctx context.Context, username string) error {
+	_, enableUserErr := a.client.AdminEnableUser(ctx,
+		&cognitoidentityprovider.AdminEnableUserInput{
+			UserPoolId: aws.String(a.userPoolID),
+			Username:   aws.String(username),
+		},
+	)
+
+	if enableUserErr != nil {
+		var userExistsError *types.UsernameExistsException
+		if errors.As(enableUserErr, &userExistsError) {
+			return &UserExistsError{
+				username: username,
+			}
+		}
+
+		return &AuthActionFailedError{
+			username:   username,
+			cause:      enableUserErr,
+			actionType: authActionEnableUser,
+		}
+	}
+
 	return nil
 }
