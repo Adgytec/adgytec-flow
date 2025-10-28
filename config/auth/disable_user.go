26e46f861113bc064c233fdf588abea2dc3aefee
rohan <rohanverma031@gmail.com>
2025-10-28 13:26:30 +0530

    feat: added stub methods for enable and disable user

---
+++ config/auth/disable_user.go
@@ -0,0 +1,7 @@
+package auth
+
+import "context"
+
+func (a *authCognito) DisableUser(ctx context.Context, username string) error {
+	return nil
+}
---
+++ config/auth/enable_user.go
@@ -0,0 +1,7 @@
+package auth
+
+import "context"
+
+func (a *authCognito) EnableUser(ctx context.Context, username string) error {
+	return nil
+}
