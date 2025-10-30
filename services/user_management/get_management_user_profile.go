0d3a129068457816ae015870676c077fa5c76b4a
rohan <rohanverma031@gmail.com>
2025-10-30 21:25:40 +0530

    feat: added method impl to get user profile for management user

--- services/user_management/get_management_user_profile.go
+++ services/user_management/get_management_user_profile.go
@@ -5,13 +5,36 @@ import (
 	"net/http"
 
 	"github.com/Adgytec/adgytec-flow/database/models"
+	"github.com/Adgytec/adgytec-flow/services/iam"
+	"github.com/Adgytec/adgytec-flow/utils/actor"
 	"github.com/Adgytec/adgytec-flow/utils/payload"
 	reqparams "github.com/Adgytec/adgytec-flow/utils/req_params"
 	"github.com/google/uuid"
 )
 
 func (s *userManagementService) getUserProfile(ctx context.Context, userID uuid.UUID) (*models.GlobalUser, error) {
-	return nil, nil
+	permissionErr := s.iam.CheckPermission(ctx,
+		iam.NewPermissionRequiredFromManagementPermission(getManagementUserProfilePermission,
+			iam.PermissionRequiredResources{},
+		),
+	)
+	if permissionErr != nil {
+		return nil, permissionErr
+	}
+
+	// check mangement user existence
+	exists, dbErr := s.db.Queries().ManagementUserExists(ctx, userID)
+	if dbErr != nil {
+		return nil, dbErr
+	}
+
+	if !exists {
+		return nil, &UserNotExistsInManagementError{}
+	}
+
+	// escale user privilage to system to get user profile from user service
+	userDetails, userErr := s.getUserProfile(actor.NewSystemActorContext(ctx), userID)
+	return userDetails, userErr
 }
 
 func (m *serviceMux) getUserProfile(w http.ResponseWriter, r *http.Request) {
