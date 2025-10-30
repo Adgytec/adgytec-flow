2f59471eaeb06b0c4d8f9deccccfcde4577d3bba
rohan <rohanverma031@gmail.com>
2025-10-30 21:12:29 +0530

    feat: added get user profile handler impl for user management service

--- services/user_management/get_management_user_profile.go
+++ services/user_management/get_management_user_profile.go
@@ -1,5 +1,30 @@
 package usermanagement
 
-import "net/http"
+import (
+	"context"
+	"net/http"
 
-func (m *serviceMux) getUserProfile(w http.ResponseWriter, r *http.Request) {}
+	"github.com/Adgytec/adgytec-flow/database/models"
+	"github.com/Adgytec/adgytec-flow/utils/payload"
+	reqparams "github.com/Adgytec/adgytec-flow/utils/req_params"
+	"github.com/google/uuid"
+)
+
+func (s *userManagementService) getUserProfile(ctx context.Context, userID uuid.UUID) (*models.GlobalUser, error) {
+	return nil, nil
+}
+
+func (m *serviceMux) getUserProfile(w http.ResponseWriter, r *http.Request) {
+	userID, userIDErr := reqparams.GetUserIDFromRequest(r)
+	if userIDErr != nil {
+		payload.EncodeError(w, userIDErr)
+		return
+	}
+
+	userProfile, userErr := m.service.getUserProfile(r.Context(), userID)
+	if userErr != nil {
+		payload.EncodeError(w, userErr)
+		return
+	}
+	payload.EncodeJSON(w, http.StatusOK, userProfile)
+}
