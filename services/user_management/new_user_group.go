1fdb3ac716562f169fa03df974f333f08b88b0d0
rohan <rohanverma031@gmail.com>
2025-10-31 12:58:56 +0530

    feat: added struct for new user group request body

--- services/user_management/new_user_group.go
+++ services/user_management/new_user_group.go
@@ -1,5 +1,29 @@
 package usermanagement
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/Adgytec/adgytec-flow/utils/core"
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+)
+
+type newUserGroupData struct {
+	Name        string  `json:"name"`
+	Description *string `json:"description"`
+}
+
+func (groupDetails newUserGroupData) Validate() error {
+	validationErr := validation.ValidateStruct(&groupDetails,
+		validation.Field(&groupDetails.Name, validation.Required),
+	)
+
+	if validationErr != nil {
+		return &core.FieldValidationError{
+			ValidationErrors: validationErr,
+		}
+	}
+
+	return nil
+}
 
 func (m *serviceMux) newUserGroup(w http.ResponseWriter, r *http.Request) {}
