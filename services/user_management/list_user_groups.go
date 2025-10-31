6b2c1da33c32f3eb81579672c1e34c02583247ea
rohan <rohanverma031@gmail.com>
2025-10-31 19:31:28 +0530

    feat: added stub method to list user groups

--- services/user_management/list_user_groups.go
+++ services/user_management/list_user_groups.go
@@ -1,5 +1,16 @@
 package usermanagement
 
-import "net/http"
+import (
+	"context"
+	"net/http"
+
+	"github.com/Adgytec/adgytec-flow/utils/pagination"
+)
+
+func (s *userManagementService) listUserGroups(ctx context.Context,
+	params pagination.PaginationRequestParams,
+) (*pagination.ResponsePagination[any], error) {
+	return nil, nil
+}
 
 func (m *serviceMux) listUserGroups(w http.ResponseWriter, r *http.Request) {}
