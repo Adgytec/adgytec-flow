5ae4249eb84bbdf2adb32c1319d11cfecd201b5b
rohan <rohanverma031@gmail.com>
2025-10-31 14:54:57 +0530

    feat: added method stub for update user group

--- services/user_management/update_user_group.go
+++ services/user_management/update_user_group.go
@@ -1,14 +1,17 @@
 package usermanagement
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"strings"
 	"unicode/utf8"
 
+	"github.com/Adgytec/adgytec-flow/database/db"
 	"github.com/Adgytec/adgytec-flow/utils/core"
 	"github.com/Adgytec/adgytec-flow/utils/types"
 	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/google/uuid"
 )
 
 type updateUserGroupData struct {
@@ -45,6 +48,10 @@ func (groupDetails updateUserGroupData) Validate() error {
 	}
 
 	return nil
+}
+
+func (s *userManagementService) updateUserGroup(ctx context.Context, groupID uuid.UUID, groupDetails updateUserGroupData) (*db.UpdateUserGroupRow, error) {
+	return nil, nil
 }
 
 func (m *serviceMux) updateUserGroup(w http.ResponseWriter, r *http.Request) {}
