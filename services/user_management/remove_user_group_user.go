222ba5b11a2e2a0288d760d0b9f6cd1073d7c07e
rohan <rohanverma031@gmail.com>
2025-10-31 22:56:39 +0530

    feat: added mux handler impl to remove user from user group

--- services/user_management/remove_user_group_user.go
+++ services/user_management/remove_user_group_user.go
@@ -1,5 +1,36 @@
 package usermanagement
 
-import "net/http"
+import (
+	"context"
+	"net/http"
 
-func (m *serviceMux) removeUserGroupUser(w http.ResponseWriter, r *http.Request) {}
+	"github.com/Adgytec/adgytec-flow/utils/payload"
+	reqparams "github.com/Adgytec/adgytec-flow/utils/req_params"
+	"github.com/google/uuid"
+)
+
+func (s *userManagementService) removeUserGroupUser(ctx context.Context, groupID, userID uuid.UUID) error {
+	return nil
+}
+
+func (m *serviceMux) removeUserGroupUser(w http.ResponseWriter, r *http.Request) {
+	userID, userIDErr := reqparams.GetUserIDFromRequest(r)
+	if userIDErr != nil {
+		payload.EncodeError(w, userIDErr)
+		return
+	}
+
+	groupID, groupIDErr := reqparams.GetUserGroupIDFromRequest(r)
+	if groupIDErr != nil {
+		payload.EncodeError(w, groupIDErr)
+		return
+	}
+
+	removeErr := m.service.removeUserGroupUser(r.Context(), groupID, userID)
+	if removeErr != nil {
+		payload.EncodeError(w, removeErr)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
