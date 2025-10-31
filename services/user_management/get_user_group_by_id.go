5752abc009612ebae844186e74854e61c8ce2f83
rohan <rohanverma031@gmail.com>
2025-10-31 21:17:49 +0530

    feat: added method stub to get user group by id

--- services/user_management/get_user_group_by_id.go
+++ services/user_management/get_user_group_by_id.go
@@ -1,5 +1,15 @@
 package usermanagement
 
-import "net/http"
+import (
+	"context"
+	"net/http"
+
+	"github.com/Adgytec/adgytec-flow/database/models"
+	"github.com/google/uuid"
+)
+
+func (s *userManagementService) getUserGroupByID(ctx context.Context, groupID uuid.UUID) (*models.UserGroup, error) {
+	return nil, nil
+}
 
 func (m *serviceMux) getUserGroupByID(w http.ResponseWriter, r *http.Request) {}
