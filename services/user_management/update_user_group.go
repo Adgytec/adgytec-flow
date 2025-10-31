317cd02b259e4409da641adf9ce758be076d5d26
rohan <rohanverma031@gmail.com>
2025-10-31 14:53:47 +0530

    feat: added request body struct for update user group

--- services/user_management/update_user_group.go
+++ services/user_management/update_user_group.go
@@ -1,5 +1,50 @@
 package usermanagement
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"unicode/utf8"
+
+	"github.com/Adgytec/adgytec-flow/utils/core"
+	"github.com/Adgytec/adgytec-flow/utils/types"
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+)
+
+type updateUserGroupData struct {
+	Name        types.NullableString `json:"name"`
+	Description types.NullableString `json:"description"`
+}
+
+func (groupDetails updateUserGroupData) Validate() error {
+	validationErr := validation.ValidateStruct(&groupDetails,
+		validation.Field(
+			&groupDetails.Name,
+			validation.By(
+				func(val any) error {
+					name := val.(types.NullableString)
+					if name.Null() {
+						return nil
+					}
+
+					nameLen := utf8.RuneCountInString(strings.TrimSpace(name.Value))
+					if nameLen < 1 {
+						return fmt.Errorf("Missing required name value")
+					}
+
+					return nil
+				},
+			),
+		),
+	)
+
+	if validationErr != nil {
+		return &core.FieldValidationError{
+			ValidationErrors: validationErr,
+		}
+	}
+
+	return nil
+}
 
 func (m *serviceMux) updateUserGroup(w http.ResponseWriter, r *http.Request) {}
