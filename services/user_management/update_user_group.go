e59f0ef4c27cf7329fb5ccd9cf6635ef235307cd
rohan <rohanverma031@gmail.com>
2025-10-31 15:38:38 +0530

    feat: added method impl for updating user group

--- services/user_management/update_user_group.go
+++ services/user_management/update_user_group.go
@@ -2,18 +2,21 @@ package usermanagement
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"strings"
 	"unicode/utf8"
 
 	"github.com/Adgytec/adgytec-flow/database/db"
+	"github.com/Adgytec/adgytec-flow/services/iam"
 	"github.com/Adgytec/adgytec-flow/utils/core"
 	"github.com/Adgytec/adgytec-flow/utils/payload"
 	reqparams "github.com/Adgytec/adgytec-flow/utils/req_params"
 	"github.com/Adgytec/adgytec-flow/utils/types"
 	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 )
 
 type updateUserGroupData struct {
@@ -57,7 +60,66 @@ func (groupDetails updateUserGroupData) Validate() error {
 }
 
 func (s *userManagementService) updateUserGroup(ctx context.Context, groupID uuid.UUID, groupDetails updateUserGroupData) (*db.UpdateUserGroupRow, error) {
-	return nil, nil
+	permissionErr := s.iam.CheckPermission(ctx,
+		iam.NewPermissionRequiredFromManagementPermission(updateUserGroupPermission,
+			iam.PermissionRequiredResources{},
+		),
+	)
+	if permissionErr != nil {
+		return nil, permissionErr
+	}
+
+	// start tx
+	qtx, tx, txErr := s.db.WithTransaction(ctx)
+	if txErr != nil {
+		return nil, txErr
+	}
+	defer tx.Rollback(context.Background())
+
+	// get existing group details
+	existingGroup, existingErr := qtx.Queries().GetUserGroupByID(ctx, groupID)
+	if existingErr != nil {
+		if errors.Is(existingErr, pgx.ErrNoRows) {
+			return nil, &UserGroupNotFoundError{}
+		}
+		return nil, existingErr
+	}
+
+	// update group obj
+	updatedGroupParams := db.UpdateUserGroupParams{
+		ID: groupID,
+	}
+
+	// name check
+	if groupDetails.Name.Missing() {
+		updatedGroupParams.Name = existingGroup.Name
+	} else {
+		// name will always be null its validated before when request body decoding
+		updatedGroupParams.Name = groupDetails.Name.Value
+	}
+
+	// description check
+	if groupDetails.Description.Missing() {
+		updatedGroupParams.Description = existingGroup.Description
+	} else if !groupDetails.Description.Null() {
+		updatedGroupParams.Description = &groupDetails.Description.Value
+	}
+
+	// update group
+	updatedGroup, dbErr := qtx.Queries().UpdateUserGroup(ctx, updatedGroupParams)
+	if dbErr != nil {
+		if errors.Is(dbErr, pgx.ErrNoRows) {
+			return nil, &UserGroupNotFoundError{}
+		}
+		return nil, dbErr
+	}
+
+	commitErr := tx.Commit(ctx)
+	if commitErr != nil {
+		return nil, commitErr
+	}
+
+	return &updatedGroup, nil
 }
 
 func (m *serviceMux) updateUserGroup(w http.ResponseWriter, r *http.Request) {
