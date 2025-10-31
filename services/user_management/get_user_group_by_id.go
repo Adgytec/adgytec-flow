1cc7b9ab6162966d8858582fb61eaab7d5c8c05a
rohan <rohanverma031@gmail.com>
2025-10-31 21:19:03 +0530

    feat: added mux handler impl to get user group by id

--- services/user_management/get_user_group_by_id.go
+++ services/user_management/get_user_group_by_id.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 
 	"github.com/Adgytec/adgytec-flow/database/models"
+	"github.com/Adgytec/adgytec-flow/utils/payload"
+	reqparams "github.com/Adgytec/adgytec-flow/utils/req_params"
 	"github.com/google/uuid"
 )
 
@@ -12,4 +14,18 @@ func (s *userManagementService) getUserGroupByID(ctx context.Context, groupID uu
 	return nil, nil
 }
 
-func (m *serviceMux) getUserGroupByID(w http.ResponseWriter, r *http.Request) {}
+func (m *serviceMux) getUserGroupByID(w http.ResponseWriter, r *http.Request) {
+	groupID, groupIDErr := reqparams.GetUserGroupIDFromRequest(r)
+	if groupIDErr != nil {
+		payload.EncodeError(w, groupIDErr)
+		return
+	}
+
+	groupDetails, groupErr := m.service.getUserGroupByID(r.Context(), groupID)
+	if groupErr != nil {
+		payload.EncodeError(w, groupErr)
+		return
+	}
+
+	payload.EncodeJSON(w, http.StatusOK, groupDetails)
+}
